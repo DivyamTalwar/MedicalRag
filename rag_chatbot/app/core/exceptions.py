5c533cff24ab45dc5f6f8eabc7b7222d24359bea
DIVYAM TALWAR <divyamtalwar0@gmail.com>
2025-09-03 23:17:47 +0530

    Refactoring The Entire codebase

--- rag_chatbot/app/core/exceptions.py
+++
@@ -1,5 +0,0 @@
-class VectorDBError(Exception):
-    def __init__(self, db_name: str, message: str):
-        self.db_name = db_name
-        self.message = message
-        super().__init__(f"Error with {self.db_name}: {self.message}")
